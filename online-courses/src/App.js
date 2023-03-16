49751509b803faf60bc674f62a8a83115f7179be
wms09291963 <98226872+wms09291963@users.noreply.github.com>
2023-03-16 13:24:48 -0400

    Created React App and added files.

---
+++ online-courses/src/App.js
@@ -0,0 +1,11 @@
+import '../src/App.css';
+
+import Gallery from '../src/views/Gallery';
+import Rationale from '../src/views/Rationale';
+
+export default function App() {
+  return (
+    <><Gallery /><Rationale /></>
+
+  );
+}
